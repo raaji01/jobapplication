c221c2223f711f1a8f2d5e8733ccaacc56891544
raaji01 <raajalakshumik18077@it.ssn.edu.in>
2023-11-15 20:02:38 -0600

    verynew

--- index.js
+++ index.js
@@ -1,41 +1,99 @@
-const express = require('express');
+const express = require("express");
 const multer = require('multer');
-const path = require('path');
+const cors = require('cors');
 const fs = require('fs');
-const app = express();
-const port = process.env.PORT || 3000;
+const path = require('path');
+var app = express();
+app.use(cors()); // Allows incoming requests from any IP
 
-// Set up Multer storage configuration
+app.set("view engine", "ejs")
+//app.use(express.static("public"));
+//app.use('/uploads',express.static('uploads'));
+
+// Keep track of uploaded filenames
+let uploadedFiles = [];
+
+// Start by creating some disk storage options:
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Set the destination folder where files will be stored
-    cb(null, path.join(__dirname, 'public/uploads'));
-  },
-  filename: function (req, file, cb) {
-    // Set the file name to be the original name of the file
-    cb(null, file.originalname);
-  },
+    destination: function (req, file, callback) {
+        const uploadPath = path.join(__dirname, '/uploads');
+        callback(null, uploadPath);
+    },
+    filename: function (req, file, callback) {
+        const filename = file.originalname;
+
+        // Check if the file already exists
+        if (uploadedFiles.includes(filename)) {
+            callback(new Error("File already exists"), null);
+        } else {
+            uploadedFiles.push(filename); // Add filename to the list
+            callback(null, filename);
+        }
+    }
 });
 
-// Create Multer instance with the configured storage
+
+app.get("/download-file", (req, res) => {
+    const filePath = path.join(__dirname, 'docs', 'RAAJALAKSHUMI_K_oncampas.pdf');
+    res.download(filePath);
+});
+
+
+
+
+// Set saved storage options:
 const upload = multer({ storage: storage });
-
-// Set up a route for file uploads
-app.post('/upload', upload.array('files'), (req, res) => {
-  // 'files' should match the name attribute in your HTML form for file input
-  req.files.forEach((f) => {
-    fs.renameSync(`${f.path}`, `public/uploads/${f.originalname}`);
-  });
-
-  // Files are uploaded, you can perform additional actions here if needed
-  res.send('Files uploaded successfully!');
-});
-
-// Set up a simple route to serve an HTML form for testing
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/uploaded-files", (req, res) => {
+    res.json({ uploadedFiles });
+});
+
+// Delete a file
+app.delete('/delete-file/:filename', (req, res) => {
+    const filenameToDelete = req.params.filename;
+    const filePath = __dirname + '/uploads/' + filenameToDelete;
+
+    // Check if the file exists
+    if (uploadedFiles.includes(filenameToDelete) && fs.existsSync(filePath)) {
+        // Delete the file
+        fs.unlinkSync(filePath);
+        // Remove the filename from the uploadedFiles array
+        uploadedFiles = uploadedFiles.filter(filename => filename !== filenameToDelete);
+        res.json({ message: "File deleted successfully" });
+    } else {
+        res.status(404).json({ message: "File not found or could not be deleted" });
+    }
+});
+
+// ... (your existing code)
+
+app.post("/api", upload.array("files"), (req, res) => {
+    console.log(req.body); // Logs form body values
+    console.log(req.files); // Logs any files
+
+    // Check for errors during upload
+    if (req.fileValidationError) {
+        return res.status(400).json({ message: req.fileValidationError });
+    }
+
+    const uploadedFiles = req.files.map(file => file.originalname);
+
+    // Inject success message and file list into the HTML
+    const successMessage = "File(s) uploaded successfully";
+    const fileListHTML = uploadedFiles.map(file => `<li>${file}</li>`).join("");
+    const script = `<script>
+                        document.getElementById('message').innerText = '${successMessage}';
+                        document.getElementById('fileList').innerHTML = '<ul>${fileListHTML}</ul>';
+                    </script>`;
+
+    res.json({ message: successMessage, uploadedFiles, script });
+});
+
+// ... (your existing code)
+
+app.listen(5000, function () {
+    console.log("Server running on port 5000");
 });
